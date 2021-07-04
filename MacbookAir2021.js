7b6266deeac5d7c30f18b4a511c6753eb3dcb41a
rajeshbandi2211 <rajeshbandi2211@gmail.com>
2021-07-04 06:14:19 +0530

    New MacbookAir2021 description

---
+++ MacbookAir2021.js
@@ -0,0 +1,8 @@
+
+Macbook Air 2021 specs and features
+
+--The Macbook Air 2021 is supposed to get some exciting new colours, as suggested by a couple of leaks. Jon Prosser's source shared Apple's MacWorld 1999 presentation that hints towards the arrival of colourful desktops and laptops. The source also talked about a Blue Macbook prototype that looked stunning.
+
+200~Photo Credit - Jon Prosser and Ian Zelbo, Macbook Air 2021 render
+	--Not just this, Jon Prosser, in collaboration with Ian Zelbo, has released a set of renders of the upcoming Macbook Air 2021 in a blue colour option. The bezels appear to be substantially reduced in the renders, as hinted in previous leaks. Also, a white keypad has been used that goes with the blue colour option. We may see Macbook Air 2021 in Yellow, Green, Silver and more colours apart from the Blue colour.
+
