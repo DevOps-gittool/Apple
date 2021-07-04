aaddd07256273e3395b1fc87c82f243caebe1371
rajeshbandi2211 <rajeshbandi2211@gmail.com>
2021-07-04 06:22:31 +0530

    The new apple ipads information

---
+++ newIpadsmodls.js
@@ -0,0 +1,16 @@
+
+With so many Apple devices floating around, it can be difficult to keep track of which tablet, computer, or phone is the most recent.
+
+	That's especially true of the iPad family, where there are not only plenty of different models, but also plenty of different generations.
+
+There are four different models of the iPad currently available: 
+
+the iPad Mini,
+
+the iPad, 
+
+the iPad Air, 
+
+and the iPad Pro. 
+
+Among the four of them, there are three different generations, and we may soon be getting yet another set of updates.
