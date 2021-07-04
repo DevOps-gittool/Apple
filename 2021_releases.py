60edfa893dd3a3421464f50d340f9c8ec863a0f4
rajeshbandi2211 <rajeshbandi2211@gmail.com>
2021-07-04 06:09:19 +0530

    This is for Applemobiles branch

---
+++ 2021_releases.py
@@ -0,0 +1,12 @@
+
+Apple Mobile Price List 2021.
+
+Apple rules the premium segment in India and around the world. The Cupertino giant has been always known for launching iPhones that have revolutionized the smartphone industry.
+
+Apple iPhones are considered the pinnacle of premium quality and top-of-the-line features. The first iPhone was introduced in 2007 and since then the company has launched a plethora of iPhones.
+
+Apple is known to provide highly-optimized software along with the iPhone range known as iOS. This coupled with premium design makes them the wannabe smartphone for all customers. 
+
+So, in order to keep you updated, we have designed this iPhone price list page where you can go through all the iPhones that are available in the market.
+
+Our Apple iPhone price list provides all the deals along with the platforms that are offering the latest iPhones at the lowest prices. On this page, you will find the latest and updated iPhone price list which has all the Apple mobiles that are currently available in India.
